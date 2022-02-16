41a85471e21bb836824d2907ea5c7763e12f040d
Aastha <aasthaarora741@gmail.com>
2022-02-16 19:26:33 +0530

    content for complaints and services andp profile pages for admin and nursery

---
+++ src/Pages/admin/profile.js
@@ -0,0 +1,161 @@
+import React, { useEffect, useState } from "react";
+import DashboardHeader from "../../Components/DashboardHeader";
+import DashboardMenu from "../../Components/DashboardMenu";
+import styled from "styled-components";
+import { IoIosArrowDown } from "react-icons/io";
+import { BsTruck, BsBell, BsPersonCircle } from "react-icons/bs";
+
+import { DashboardCard } from "../../Components/Dashboard Items/DashboardElements";
+
+import {
+  AddProductsForm,
+  Wrapper1,
+  Label,
+  Input,
+  ProductDescription,
+  DashboardButton,
+} from "../../Components/DashboardInputs";
+
+import {
+  CustomOption,
+  CustomOptions,
+  Select,
+  SelectLabel,
+  SelectTrigger,
+  SelectWrapper,
+} from "../../Components/NurseryFormElements";
+import { NurseryMenu } from "../../data/dashboard-menu-items";
+
+const UserName = styled.p`
+  font-size: 1.5rem;
+  color: black;
+`;
+
+const Container = styled.section`
+  width: 100vw;
+  height: calc(100vh - 60px);
+  overflow-y: scroll;
+  background-color: #fbfbfb;
+  padding: 2.5rem 1.5rem;
+  position: relative;
+
+  @media (min-width: 1100px) {
+    width: calc(100vw - 275px);
+    left: 275px;
+  }
+`;
+const ChangePasswaord = styled.p`
+  font-size: 1.1rem;
+  color: #066093;
+  cursor: pointer;
+`;
+
+const Title = styled.h4`
+  font-size: 1.1rem;
+  color: #444;
+  font-weight: 500;
+  margin: 1rem;
+`;
+const Icons = styled.div`
+  color: #666;
+  font-size: 4rem;
+  display: flex;
+  margin-top: 1rem;
+  margin-left: 1rem;
+
+  align-items: center;
+
+  & span {
+    color: #212121;
+    margin-right: 0.75rem;
+    font-size: 1rem;
+
+    @media (max-width: 540px) {
+      display: none;
+    }
+  }
+
+  & svg {
+    margin-right: 1.5rem;
+    @media (max-width: 540px) {
+      font-size: 1rem;
+      margin-right: 1rem;
+    }
+  }
+
+  & svg:last-child {
+    margin: 0;
+  }
+`;
+
+const AdminProfile = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const openDropdown = (e) => {
+    e.target.closest(".select").classList.toggle("open");
+  };
+
+  const changeSelection = (e) => {
+    const el = e.target;
+
+    if (selectedOption !== "") {
+      const siblings = Array.from(e.target.parentNode.childNodes);
+      const selectedSibling = siblings.filter((el) =>
+        el.classList.contains("selected")
+      );
+      selectedSibling[0].classList.remove("selected");
+    }
+    setSelectedOption(el.innerText);
+    el.classList.add("selected");
+  };
+
+  useEffect(() => {
+    window.innerWidth >= 1100 ? setMenuOpen(true) : setMenuOpen(false);
+  }, [setMenuOpen]);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
+  return (
+    <>
+      <DashboardHeader toggleMenu={toggleMenu} />
+      <DashboardMenu menuOpen={menuOpen} listItems={NurseryMenu} />
+      <Container>
+        <DashboardCard style={{ padding: "1rem" }}>
+          <Title>Your Account</Title>
+          <Icons>
+            <BsPersonCircle />
+            <UserName>Admin</UserName>
+          </Icons>
+          <AddProductsForm style={{ marginTop: "1.5rem" }}>
+            <Wrapper1>
+              <Label>First Name</Label>
+              <Input spellcheck="false" type="email" name="fname" />
+            </Wrapper1>
+            <Wrapper1>
+              <Label>Last Name</Label>
+              <Input spellcheck="false" type="text" name="lname" />
+            </Wrapper1>
+            <Wrapper1>
+              <Label>Email</Label>
+              <Input spellcheck="false" type="text" name="email" />
+            </Wrapper1>
+            <Wrapper1>
+              <Label>Phone No</Label>
+              <Input spellcheck="false" type="text" name="phone" />
+            </Wrapper1>
+            <Wrapper1 style={{ width: "100%" }}>
+              <ChangePasswaord>Change Password</ChangePasswaord>
+            </Wrapper1>
+            <div>
+              <DashboardButton className="primary">Save</DashboardButton>
+              <DashboardButton className="cancel">Cancel</DashboardButton>
+            </div>
+          </AddProductsForm>
+        </DashboardCard>
+      </Container>
+    </>
+  );
+};
+
+export default AdminProfile;
--- src/data/complaints.js
+++ src/data/complaints.js
@@ -3,7 +3,7 @@ export const complaints = [
     id: "#90231",
     subject: "Late Delivery",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga rem corrupti nulla provident officia veritatis neque vitae ipsum omnis totam?",
+      "The delivery was scheduled two days back and I haven’t received the order yet. The items in the order were a priority one and as you promised on-time delivery I agreed to proceed for the transaction.",
     issuedBy: "Mitul Parikh",
     issueDate: "20/01/2022",
     status: "Unresolved",
@@ -13,7 +13,7 @@ export const complaints = [
     id: "#90232",
     subject: "Unsatisfactory Service",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga rem corrupti nulla provident officia veritatis neque vitae ipsum omnis totam?",
+      "I wish to complain about the plants that I purchased on 2/2/2022. I am complaining because the plants I recieved were all dried. To resolve this problem I would like you to give me another delivery for the same plants",
     issuedBy: "Pujan Adani",
     issueDate: "19/01/2022",
     status: "Unresolved",
@@ -23,7 +23,7 @@ export const complaints = [
     id: "#90233",
     subject: "Wrong Delivery",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga rem corrupti nulla provident officia veritatis neque vitae ipsum omnis totam?",
+      " I received the wrong product in the delivery made on 2/2/2022(Mention delivery date),Therefore I am returning the order and request you to kindly send me the correct product at the earliest.",
     issuedBy: "Aditya Dalal",
     issueDate: "20/01/2022",
     status: "Resolved",
@@ -33,7 +33,7 @@ export const complaints = [
     id: "#90234",
     subject: "Late Delivery",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga rem corrupti nulla provident officia veritatis neque vitae ipsum omnis totam?",
+      "The delivery was scheduled two days back and I haven’t received the order yet. The items in the order were a priority one and as you promised on-time delivery I agreed to proceed for the transaction.",
     issuedBy: "Mitul Parikh",
     issueDate: "20/01/2022",
     status: "Resolved",
@@ -43,7 +43,7 @@ export const complaints = [
     id: "#90235",
     subject: "Late Delivery",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga rem corrupti nulla provident officia veritatis neque vitae ipsum omnis totam?",
+      "The delivery was scheduled two days back and I haven’t received the order yet. The items in the order were a priority one and as you promised on-time delivery I agreed to proceed for the transaction.",
     issuedBy: "Mitul Parikh",
     issueDate: "20/01/2022",
     status: "Resolved",
@@ -53,7 +53,7 @@ export const complaints = [
     id: "#90236",
     subject: "Late Delivery",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga rem corrupti nulla provident officia veritatis neque vitae ipsum omnis totam?",
+      "The delivery was scheduled two days back and I haven’t received the order yet. The items in the order were a priority one and as you promised on-time delivery I agreed to proceed for the transaction.",
     issuedBy: "Mitul Parikh",
     issueDate: "20/01/2022",
     status: "Resolved",
@@ -63,7 +63,7 @@ export const complaints = [
     id: "#90237",
     subject: "Late Delivery",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga rem corrupti nulla provident officia veritatis neque vitae ipsum omnis totam?",
+      "The delivery was scheduled two days back and I haven’t received the order yet. The items in the order were a priority one and as you promised on-time delivery I agreed to proceed for the transaction.",
     issuedBy: "Mitul Parikh",
     issueDate: "20/01/2022",
     status: "Resolved",
@@ -73,7 +73,7 @@ export const complaints = [
     id: "#90238",
     subject: "Late Delivery",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga rem corrupti nulla provident officia veritatis neque vitae ipsum omnis totam?",
+      "The delivery was scheduled two days back and I haven’t received the order yet. The items in the order were a priority one and as you promised on-time delivery I agreed to proceed for the transaction.",
     issuedBy: "Mitul Parikh",
     issueDate: "20/01/2022",
     status: "Resolved",
@@ -83,7 +83,7 @@ export const complaints = [
     id: "#90239",
     subject: "Late Delivery",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga rem corrupti nulla provident officia veritatis neque vitae ipsum omnis totam?",
+      "The delivery was scheduled two days back and I haven’t received the order yet. The items in the order were a priority one and as you promised on-time delivery I agreed to proceed for the transaction.",
     issuedBy: "Mitul Parikh",
     issueDate: "20/01/2022",
     status: "Resolved",
